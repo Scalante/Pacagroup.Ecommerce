e5b1d7aca4cdd29ecc7a9fb72a4e9c14e0d3cf8c
RubenScalante <RubenScalante@DESKTOP-Q66425S>
2022-08-31 01:08:40 -0500

    Se implementa las carpetas  del DDD y se crean algunos proyectos de biblioteca de clases

---
+++ Pacagroup.Ecommerce.Infrastructure.Interface/ICustomersRepository.cs
@@ -0,0 +1,31 @@
+﻿using Pacagroup.Ecommerce.Domain.Entity;
+using System.Threading.Tasks;
+
+namespace Pacagroup.Ecommerce.Infrastructure.Interface
+{
+    public interface ICustomersRepository
+    {
+        #region Métodos Síncronos
+
+        bool Insert(Customers customers);
+        bool Update(Customers customers);
+        bool Delete(string customerId);
+        Customers Get(string customerId);
+        IEnumerable <Customers> GetAll();
+
+        #endregion
+
+
+        #region Métodos Asíncronos
+
+        Task<bool> InsertAsync(Customers customers);
+        Task<bool> UpdateAsync(Customers customers);
+        Task<bool> DeleteAsync(string customerId);
+        Task<Customers> GetAsync(string customerId);
+        Task<IEnumerable<Customers>> GetAllAsync();
+
+        #endregion
+
+
+    }
+}
---
+++ Pacagroup.Ecommerce.Infrastructure.Repository/CustomersRepository.cs
@@ -0,0 +1,89 @@
+﻿using Pacagroup.Ecommerce.Domain.Entity;
+using Pacagroup.Ecommerce.Infrastructure.Interface;
+using Pacagroup.Ecommerce.Transversal.Common;
+using Dapper;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace Pacagroup.Ecommerce.Infrastructure.Repository
+{
+    public class CustomersRepository : ICustomersRepository
+    {
+        private readonly IConnectionFactory _connectionFactory;
+
+        public CustomersRepository(IConnectionFactory connectionFactory)
+        {
+            this._connectionFactory = connectionFactory;
+        }
+
+       
+        #region Métodos Síncronos
+
+        public bool Insert(Customers customers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Customers> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customers Get(string customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(string customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Customers customers)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+
+
+        #region Métodos Asíncronos
+
+
+
+
+        public Task<IEnumerable<Customers>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customers> GetAsync(string customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        public Task<bool> InsertAsync(Customers customers)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        public Task<bool> UpdateAsync(Customers customers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(string customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+
+    }
+}
